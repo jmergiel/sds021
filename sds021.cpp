4a80c35e38e5589f29b8b5d7ac9a2ec1a311b4bc
Jakub Mergiel <jmergiel@q-loc.com>
2017-05-13 17:00:02 +0200

    chmod -x on sds021.cpp; added Options parsing; some fixes

--- sds021.cpp
+++ sds021.cpp
@@ -5,6 +5,8 @@
 #include <termios.h>
 #include <unistd.h>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 #ifdef INTERACTIVE
 #include <thread>
@@ -59,6 +61,29 @@ namespace Cmd
 	static constexpr int Tail = 9;
 }
 
+namespace Options
+{
+	// I need it simple and fast so found this on StackOverflow.com and altered a bit
+	// http://stackoverflow.com/questions/865668/how-to-parse-command-line-arguments-in-c
+	class InputParser
+	{
+		std::vector <std::string> tokens;
+		public:
+			InputParser(int argc, const char** argv)
+			{
+				for(int i=1; i < argc; ++i) tokens.emplace_back(argv[i]);
+			}
+
+			bool cmdOptionExists(const std::string& option) const
+			{
+				return std::find(tokens.begin(), tokens.end(), option) != tokens.end();
+			}
+	};
+
+	static bool bInteractiveMode = false;
+	static bool bJustNumbers = false;
+}
+
 std::string parseReply(const uint8_t (&V)[RESULT_BYTES])
 {
 	if(V[Cmd::CmdID] != CMDID_REPLY) return error_message("not a command reply"), "";
@@ -75,7 +100,7 @@ std::string parseReply(const uint8_t (&V)[RESULT_BYTES])
 	return buff;
 }
 
-void parse(const uint8_t (&V)[RESULT_BYTES])
+void parse(const uint8_t (&V)[RESULT_BYTES], bool bJustNumbers)
 {
 	if(V[Cmd::Head] != 0xAA) error_message("header mismatch");
 	// 0xC0: result of measurement
@@ -92,18 +117,19 @@ void parse(const uint8_t (&V)[RESULT_BYTES])
 	{
 		const float PM2_5 = (V[3] * 256 + V[2])/10.f;
 		const float PM10 = (V[5] * 256 + V[4])/10.f;
-		printf("-> MEAS PM25=%05.01f PM10=%05.01f ID=%02X%02X\n", PM2_5, PM10, V[Cmd::ID1], V[Cmd::ID2]);
+		if(bJustNumbers) printf("%.01f %.01f\n", PM2_5, PM10);
+		else printf("-> MEAS PM25=%05.01f PM10=%05.01f ID=%02X%02X\n", PM2_5, PM10, V[Cmd::ID1], V[Cmd::ID2]);
 	}
 	else
 	{
-		printf("-> RPLY d1=%02x, d2=%02x, d3=%02x, d4=%02x ID=%02X%02X <%s>\n", V[2], V[3], V[4], V[5], V[Cmd::ID1], V[Cmd::ID2], parseReply(V).c_str());
+		if(!bJustNumbers) printf("-> RPLY d1=%02x, d2=%02x, d3=%02x, d4=%02x ID=%02X%02X <%s>\n", V[2], V[3], V[4], V[5], V[Cmd::ID1], V[Cmd::ID2], parseReply(V).c_str());
 	}
 }
 
 namespace Request
 {
 	constexpr uint8_t GetPeriod[] =		{ 0xAA, 0xB4, 0x08, 0x00, 0x00, 0,0,0,0,0,0,0,0,0,0, 0xAB, 0x62, 0x15, 0xAB };
-	constexpr uint8_t SetPeriodCont[] =	{ 0xAA, 0xB4, 0x08, 0x01, 0x00, 0,0,0,0,0,0,0,0,0,0, 0xAB, 0x62, 0x16, 0xAB };	// 0 == continous mode
+	constexpr uint8_t SetPeriodCont[] =	{ 0xAA, 0xB4, 0x08, 0x01, 0x00, 0,0,0,0,0,0,0,0,0,0, 0xAB, 0x62, 0x16, 0xAB };	// 0 == continuous mode
 	constexpr uint8_t SetPeriod1m[] =	{ 0xAA, 0xB4, 0x08, 0x01, 0x01, 0,0,0,0,0,0,0,0,0,0, 0xAB, 0x62, 0x17, 0xAB };	// [4] <- n (1-30) minutes
 	constexpr uint8_t SetPeriod5m[] =	{ 0xAA, 0xB4, 0x08, 0x01, 0x05, 0,0,0,0,0,0,0,0,0,0, 0xAB, 0x62, 0x1B, 0xAB };	// 5 minutes
 	
@@ -146,13 +172,13 @@ int set_interface_attribs(int fd,int speed,int parity)
 
 	if(tcsetattr(fd,TCSANOW,&tty) != 0)
 	{
-		error_message("error %d from tcsetattr",errno);
+		error_message("tcsetattr failed (errno=%d)",errno);
 		return -1;
 	}
 	return 0;
 }
 
-void SendCmd(const uint8_t (&V)[CMD_BYTES], int fd)
+bool SendCmd(const uint8_t (&V)[CMD_BYTES], int fd)
 {
 	int check=0;
 	for(int i=2; i<=16; ++i) check += V[i];
@@ -163,20 +189,21 @@ void SendCmd(const uint8_t (&V)[CMD_BYTES], int fd)
 		printf("Sending command: ");
 		dump(V, sizeof(V));
 		printf("\n");
-		if(write(fd, V, sizeof(V)) == sizeof(V))
+		if(write(fd, V, sizeof(V)) != sizeof(V))
 		{
-			printf("Wrote command, waiting for reply...\n");
+			error_message("write failed (errno=%d)", errno);
+			return false;
 		}
 	}
+	return true;
 }
 
 void ReadThread(int fd, int pip = -1)
 {
-	//SendCmd(Request::SetModeSleep, fd);
 	for(;;)
 	{
-		uint8_t buf[RESULT_BYTES];
-
+		// this entire pip cruft is only used in interactive mode
+		// to stop this thread in a correct, on-demand fashion
 		if(pip != -1)
 		{
 			fd_set set;
@@ -191,23 +218,29 @@ void ReadThread(int fd, int pip = -1)
 			}
 			if(FD_ISSET(pip, &set))
 			{
-				//printf("pipe! so quit\n");
+				// we got something through the pipe! we care not
+				// what it is -> this is a signal we should return
 				return;
 			}
-			//if(FD_ISSET(fd, &set)) printf("COM data\n");
+			// if we're here it means there's data to be read from
+			// the serial port - we just "fall-through"
 		}
 		
+		uint8_t buf[RESULT_BYTES]{};
 		const int nBytes = read(fd, buf, sizeof(buf));
-		//printf("nBytes=%d\n", nBytes);
 		if(nBytes == RESULT_BYTES)
 		{
-			printf("%s: ", MakeTimestamp().c_str());
-			dump(buf, sizeof(buf));
-			parse(buf);
+			if(Options::bJustNumbers) parse(buf, true);
+			else
+			{
+				printf("{%s} ", MakeTimestamp().c_str());
+				dump(buf, sizeof(buf));
+				parse(buf, false);
+			}
 		}
-		else if(nBytes < 0)
+		else
 		{
-			//printf("negative - bye\n");
+			error_message("read failed with result=%d (errno=%d)", nBytes, errno);
 			return;
 		}
 	}
@@ -216,37 +249,56 @@ void ReadThread(int fd, int pip = -1)
 #ifdef INTERACTIVE
 void Interactive(int fd)
 {
-	for(;;)
+	bool bOK = true;
+	while(bOK)
 	{
 		switch(getchar())
 		{
 		case 'q':
 		case 'Q': return;
-		case 's': SendCmd(Request::SetModeSleep, fd); break;
-		case 'w': SendCmd(Request::SetModeWork, fd); break;
-		case 'p': SendCmd(Request::GetPeriod, fd); break;
-		case 'c': SendCmd(Request::SetPeriodCont, fd); break;
-		case '1': SendCmd(Request::SetPeriod1m, fd); break;
-		case '5': SendCmd(Request::SetPeriod5m, fd); break;
+		case 's': bOK = SendCmd(Request::SetModeSleep, fd); break;
+		case 'w': bOK = SendCmd(Request::SetModeWork, fd); break;
+		case 'p': bOK = SendCmd(Request::GetPeriod, fd); break;
+		case 'c': bOK = SendCmd(Request::SetPeriodCont, fd); break;
+		case '1': bOK = SendCmd(Request::SetPeriod1m, fd); break;
+		case '5': bOK = SendCmd(Request::SetPeriod5m, fd); break;
 		};
 	}
 }
 #endif
 
-int main()
+int main(int argc, const char** argv)
 {
-	const char *portname = "/dev/ttyUSB0";
+	if(argc < 2)
+	{
+		printf("\nUSAGE:\t\t%s SERIAL_PORT_PATH [OPTIONS]\n", argv[0]);
+		printf("EXAMPLE:\t%s /dev/ttyUSB0\n\n", argv[0]);
+		printf("OPTIONS:\n");
+		printf("\t-i\tInteractive mode on (default is off)\n");
+		printf("\t-n\tOutput only (space separated) values of PM2.5 first and PM10 later (default is full protocol dump)\n");
+		printf("\n\n");
+		return -2;
+	}
+
+	const char* portname = argv[1];
+	Options::InputParser cmd(argc-1, argv+1);
+	Options::bInteractiveMode = cmd.cmdOptionExists("-i");
+	Options::bJustNumbers = cmd.cmdOptionExists("-n");
+
+	// The entire serial port code is from StackOverflow.com
+	// with some modifications. I couldn't be bothered to learn
+	// using it on Linux so if there's something wrong do tell!
 	const int fd = open(portname, O_RDWR | O_NOCTTY | O_SYNC);
 	if(fd < 0)
 	{
-		error_message("error %d opening %s: %s", errno, portname, strerror(errno));
+		const int err = errno;
+		error_message("Cannot open '%s': %s (error=%d)", portname, strerror(err), err);
 		return -1;
 	}
 	auto scopedUART = make_scoped([=]{ close(fd); });
-	set_interface_attribs(fd, B9600, 0);  // set speed to 9600, 8n1 (no parity)
+	set_interface_attribs(fd, B9600, 0);  //< set speed to 9600, 8n1 (no parity)
 
-	bool bInteractive = true;
-	if(!bInteractive) ReadThread(fd);
+	if(!Options::bInteractiveMode) ReadThread(fd);
 	else
 	{
 #ifdef INTERACTIVE
@@ -261,14 +313,13 @@ int main()
 		
 		std::thread reader(ReadThread, fd, pip[0]);
 		Interactive(fd);
-		static_cast<void>(1+write(pip[1], "Q", 1));	//< funky way of suppressing the unused return value warning
+		static_cast<void>(1+write(pip[1], "Q", 1));	//< the +1 is a funky way of suppressing the unused return value warning
 		reader.join();
 #else
 		error_message("Not compiled with interactive mode enabled! Use 'make interactive' to do that.");
 #endif
 	}
 
-	printf("BYE BYE\n");
 	return 0;
 }
 
